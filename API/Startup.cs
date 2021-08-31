0c47c0936a7c396fbf208a6c677090c5b0529149
Abu Rayhan <rayhan.cse15@gmail.com>
2021-08-31 19:32:56 +0600

    cros policy added

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,14 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CrosPolicy",
+                        builder =>
+                        {
+                            builder.WithOrigins("http://localhost:3000", "http://www.contoso.com");
+                        });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +49,7 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors("CrosPolicy");
 
             app.UseEndpoints(endpoints =>
             {
