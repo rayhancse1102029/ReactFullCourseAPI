97bcbda08cb9387c6222fced17e446e0d323bd7e
Abu Rayhan <rayhan.cse15@gmail.com>
2021-09-02 18:32:47 +0600

    get all user api add

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data.Entity;
+using API.Services.Auth.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService _userService)
+        {
+            this._userService = _userService;
+        }
+
+        [HttpGet]
+        [Route("GetAllUserList")]
+        public async Task<IActionResult> GetAllUserList()
+        {
+            IEnumerable<ApplicationUser> userList = await _userService.GetAllUser();
+
+            return Ok(userList);
+        }
+
+    }
+}
